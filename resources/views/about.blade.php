aad54af72743046ae84da44a6dea996322a7d321
Sonali Hirave <hiravesonali@gmail.com>
2020-05-17 20:55:05 +0200

    route to file

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -29,60 +29,7 @@
             @endif
 
         
-            <nav class="flex items-center justify-between flex-wrap bg-purple-600 p-6">
-  <div class="flex items-center flex-shrink-0 text-white mr-6">
-    <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-    <span class="font-semibold text-2xl tracking-tight">LaraBlog</span>
-  </div>
-  <div class="block lg:hidden">
-    <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-    </button>
-  </div>
-  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-    <div class="text-base lg:flex-grow">
-      <a href="#responsive-header" class="block ml-8 mt-4 lg:inline-block lg:mt-0 text-gray-300 font-medium hover:text-white mr-4">
-        Home
-      </a>
-      <a href="#responsive-header" class="block ml-8 mt-4 lg:inline-block lg:mt-0 text-gray-300 font-medium hover:text-white mr-4">
-        Topics
-      </a>
-      <a href="#responsive-header" class="block ml-8 mt-4 lg:inline-block lg:mt-0 text-white font-semibold hover:text-white">
-        About
-      </a>
-      <a href="#responsive-header" class="block ml-8 mt-4 lg:inline-block lg:mt-0 text-gray-300 font-medium hover:text-white">
-        Contact
-      </a>
-    </div>
-
-    <div class="flex-1 flex items-center justify-center px-2">
-        <div class="max-w-lg w-full lg:max-w-xs">
-          <label for="search" class="sr-only">Search</label>
-          <div class="relative">
-            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-              <svg class="h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-                <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-              </svg>
-            </div>
-            <input id="search" class="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:border-blue-300 focus:shadow-outline-blue sm:text-sm transition duration-150 ease-in-out" placeholder="Search" type="search" />
-          </div>
-        </div>
-      </div>
-
-      <div class="flex">
-            <div>
-              <button class="flex text-lg border-transparent text-white rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out" id="user-menu" aria-label="User menu" aria-haspopup="true">
-              <svg class="h-8 w-8" fill="currentColor" viewBox="0 0 20 20"><path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path></svg>
-              </button>
-            </div>
-      <div class="ml-3 relative">
-            <div>
-              <button class="flex text-lg border-transparent text-white rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out" id="user-menu" aria-label="User menu" aria-haspopup="true">
-                <svg class="h-8 w-8" fill="currentColor" viewBox="0 0 20 20"><path d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
-              </button>
-            </div>
-  </div>
-</nav>
+        @include('partials.navigation')
                 
          <!--blog post start-->
       <div class="flex mb-4 mt-16">
--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/topics', function () {
     return view('topics');
-});
+})->name('topics');
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
 
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
