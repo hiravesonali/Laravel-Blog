4ad5eb41109c6ca3ae264543fbc47624a77e3ce9
Sonali Hirave <hiravesonali@gmail.com>
2020-06-26 17:47:03 +0200

    added route

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/topics', function () {
 Route::get('/about', function () {
     return view('about');
 })->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
